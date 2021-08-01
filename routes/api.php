0ec622633d05a2699eb2c2e03aeb0bd56b7635d7
VaniaRaigosa <wendy.vania.raigosa@gmail.com>
2021-08-01 18:52:06 -0500

    Se agregó comentario en el arhivo api

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*Categorías*/
 /*INICIAR SESIÓN*/ 
 Route::post('/login',[loginController::class,'sesion']);
 
